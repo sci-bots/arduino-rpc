99545f96c6dd6ceed848f2b0381190453f6a51c6
Christian Fobel <christian@fobel.net>
2015-06-19 17:25:46 -0400

    Add code generation support for multiple headers
     - Add code generation support for multiple C++ headers/classes.
     - Each header is allocated a block of 48 command codes (3 * 0x30).

--- arduino_rpc/code_gen.py
+++ arduino_rpc/code_gen.py
@@ -1,19 +1,38 @@
 import sys
+import types
 
+from path_helpers import path
+import pandas as pd
 from clang_helpers import open_cpp_source, extract_class_declarations
 from clang_helpers.data_frame import get_clang_methods_frame
 from .rpc_data_frame import get_struct_sig_info_frame
 
 
-def write_code(cpp_header, class_name, out_file, f_get_code):
-    root = open_cpp_source(cpp_header)
-    node_class = extract_class_declarations(root)[class_name]
+def write_code(cpp_header, class_name, out_file, f_get_code, *args, **kwargs):
+    if isinstance(cpp_header, types.StringTypes):
+        cpp_header = [cpp_header]
+    if isinstance(class_name, types.StringTypes):
+        class_name = [class_name]
+    assert(len(cpp_header) == len(class_name))
 
-    df_sig_info = get_clang_methods_frame(node_class, std_types=True)
-    df_sig_info.head()
+    frames = []
+    for header, class_ in zip(cpp_header, class_name):
+        root = open_cpp_source(header, *args, **kwargs)
+        node_class = extract_class_declarations(root)[class_]
 
-    df_struct_sig_info = get_struct_sig_info_frame(df_sig_info)
-    df_struct_sig_info.method_i += 0x80
+        df_sig_info = get_clang_methods_frame(node_class, std_types=True)
+
+        frame = get_struct_sig_info_frame(df_sig_info)
+        frame.insert(0, 'header_name', path(header).name)
+        frames.append(frame)
+
+    df_struct_sig_info = pd.concat(frames)
+
+    header_i = pd.Series(df_struct_sig_info.header_name.unique())
+    header_i = pd.Series(header_i.index, index=header_i)
+    df_struct_sig_info.method_i += 0x30 * header_i[df_struct_sig_info
+                                                   .header_name].values
+
 
     if out_file == '-':
         # Write code to `stdout`.
