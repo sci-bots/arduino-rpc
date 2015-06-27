92cfd3dd0870a75eb95f3e827526ad04d85f94bc
Christian Fobel <christian@fobel.net>
2015-06-26 20:17:52 -0400

    Use MANIFEST.in recursive-include rather than package_data

--- pavement.py
+++ pavement.py
@@ -2,15 +2,10 @@ from pprint import pprint
 import sys
 
 from paver.easy import task, needs, path, sh, cmdopts, options
-from paver.setuputils import setup, find_package_data
+from paver.setuputils import setup
 
 sys.path.insert(0, '.')
 import version
-
-
-arduino_rpc_files = find_package_data(package='arduino_rpc',
-                                      where='arduino_rpc',
-                                      only_in_packages=False)
 
 
 setup(name='wheeler.arduino_rpc',
@@ -21,5 +16,4 @@ setup(name='wheeler.arduino_rpc',
       url='http://github.com/wheeler-microfluidics/arduino_rpc.git',
       license='GPLv2',
       install_requires=['nadamq', 'path_helpers', 'clang_helpers>=0.2.post3'],
-      packages=['arduino_rpc'],
-      package_data=arduino_rpc_files)
+      packages=['arduino_rpc'])
