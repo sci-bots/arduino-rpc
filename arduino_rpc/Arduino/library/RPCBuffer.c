e81170d20bc209aa28e2fd5ae4b5d342f1123988
Christian Fobel <christian@fobel.net>
2015-06-30 12:11:45 -0400

    Use PACKET_SIZE as default for I2C_PACKET_SIZE...
     - Use `PACKET_SIZE` as default for `I2C_PACKET_SIZE`.
     - Remove `command_array_buffer` and `i2c_packet_buffer`.
       * `command_array_buffer` is deprecated, since command processor
         returns a `UInt8Array` that can point to any location in memory.
       * `i2c_packet_buffer` is no longer required, since the I2C packet is
         owned by the I2C handler instead.

--- arduino_rpc/Arduino/library/RPCBuffer.c
+++ arduino_rpc/Arduino/library/RPCBuffer.c
@@ -6,7 +6,3 @@
 #ifndef DISABLE_SERIAL
 uint8_t packet_buffer[PACKET_SIZE];
 #endif  // #ifndef DISABLE_SERIAL
-
-/*  - Allocate buffer for command-processor to extract/write array data. */
-uint8_t command_array_buffer[COMMAND_ARRAY_BUFFER_SIZE];
-uint8_t i2c_packet_buffer[I2C_PACKET_SIZE];
